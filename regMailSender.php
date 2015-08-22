851ad2393e6f350c5940976d1535f2ee7cf041f7
Amit Prasad <amitprasad53121@gmail.com>
2015-08-23 01:29:56 +0530

    registration done

--- regMailSender.php
+++ regMailSender.php
@@ -25,11 +25,12 @@ $mail->From = $_POST["email_of_candidate"];
 $mail->FromName = $_POST["name_of_candidate"];
 
 $mail->addReplyTo('aces.proxy.mail@gmail.com','Reply Address');
+$mail->addAddress('aces.dypiet@gmail.com','Acunetix 2k15');
 $mail->addAddress('aces.proxy.mail@gmail.com','Acunetix 2k15');
 
-$subject = "Name: ".$_POST['name_of_candidate']."\nContact: ".$_POST['contact_of_candidate']."\nEvent: ".$_POST['event_of_candidate'];
-$mail->Subject = "Registration:\n\n".$subject;
-$mail->Body = "This is the body of the email";
+$body = "<b>Name:</b> ".$_POST['name_of_candidate']."<br><b>Contact: </b>".$_POST['contact_of_candidate']."<br><b>Event: </b>".$_POST['event_of_candidate']."<br>";
+$mail->Subject = "Registration:  ".$_POST['event_of_candidate'];
+$mail->Body = "<b>#Registration</b><br><br>".$body;
 $mail->AltBody = "This is the body of the email";
 
 
